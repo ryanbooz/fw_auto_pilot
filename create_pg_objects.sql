93ad601b429eed72748508a83d10c35bb7354c03
Ryan Booz <ryan@softwareandbooz.com>
2024-02-21 16:38:33 -0500

    initial redo of PG Autopilot

--- create_pg_objects.sql
+++ create_pg_objects.sql
@@ -1,11 +1,3 @@
-create database WidgetDev with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-create database WidgetTest with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-create database WidgetProd with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-create database WidgetCheck with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-create database WidgetShadow with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-create database WidgetBuild with encoding 'UTF8' LC_COLLATE='English_United Kingdom' LC_CTYPE='English_United Kingdom';
-
-
 /*
  * Create necessary objects
  */
